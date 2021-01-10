b2f7bb65eccc3c92a6ba4f902772133cdaa70a02
patirosa95 <patirosa995@gmail.com>
2021-01-10 19:12:35 -0300

    added formik to component FormLogin

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,5 +1,7 @@
 import {useState} from 'react';
 
+import { ErrorMessage, Formik, Form, Field } from 'formik';
+
 function initialState() {
     return {user: '', password:''};
 }
@@ -25,17 +27,21 @@ function FormLogin(){
     }
 
     return(
-        <form onSubmit={handleSubmit} className="form-login" style={{display: 'flex', flexDirection: 'column', width: 200}}>
-            <div className="user-login">
-                <label htmlFor="user">Enter user</label>
-                <input type="text" name="user" id="user" onChange={onChange} value={values.user}/>
-            </div>
-            <div className="user-password">    
-                <label htmlFor="password">Enter password</label>
-                <input type="password" name="password" id="password" onChange={onChange} value={values.password}/>
-            </div>
-            <button type="submit" className="btn-primary">Submit</button>
-        </form>
+        <Formik>
+            <Form onSubmit={handleSubmit} className="form-login" style={{display: 'flex', flexDirection: 'column', width: 200}}>
+                <h1>Login</h1>
+                <p>Fill the fields to continue</p>
+                <div className="user-login">
+                    <label htmlFor="user">Enter user</label>
+                    <input type="text" name="user" id="user" onChange={onChange} value={values.user}/>
+                </div>
+                <div className="user-password">    
+                    <label htmlFor="password">Enter password</label>
+                    <input type="password" name="password" id="password" onChange={onChange} value={values.password}/>
+                </div>
+                <button type="submit" className="btn-primary">Submit</button>
+            </Form>
+        </Formik>
      )
 };
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 //import FormLogin from '../components/FormLogin';
 import Routes from '../components/Routes';
-
+ import './App.css'
 
 function App() {
   return (
+    <main className="App">
       <Routes/>
+    </main>
 );
 }
 
